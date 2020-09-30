66f9b36c0c45a4676559fde97eeed904494743dd
jpowell <powell.joshua@epa.gov>
2020-09-30 13:14:05 -0400

    Initial bare bones build

---
+++ src/main/java/com/app/arcx/ArcxApplication.java
@@ -0,0 +1,13 @@
+package com.app.arcx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/arcx/ArcxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.arcx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
