570cbad726400cf95ec03ad1a9417a2ba0519bc6
jpowell <powell.joshua@epa.gov>
2020-10-16 12:29:06 -0400

    Adding configurations for Spring Data Rest

--- src/main/java/com/app/arcx/repository/AreaOfInterestRepository.java
+++ src/main/java/com/app/arcx/repository/AreaOfInterestRepository.java
@@ -4,7 +4,7 @@ import com.app.arcx.domain.AreaOfInterest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "areaofinterest", path = "areaofinterest")
+@RepositoryRestResource(collectionResourceRel = "area_of_interest", path = "area_of_interest")
 public interface AreaOfInterestRepository extends CrudRepository<AreaOfInterest, Integer> {
 
 }
