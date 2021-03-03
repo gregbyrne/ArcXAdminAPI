b9e92a24c6cf60894e58dd9b2544d8a226b06179
greg19735 <greg19735@gmail.com>
2021-03-03 12:18:56 -0500

    WDT-643 STHP Sorting API fixes
    add sthp.position and findAll function to sort STHP by position

--- src/main/java/com/app/arcx/domain/StepsToHelpPrepare.java
+++ src/main/java/com/app/arcx/domain/StepsToHelpPrepare.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 @Entity
 @Data
 @Table(name = "steps_to_help_prepare")
+@NamedQuery(name = "StepsToHelpPrepare.findAll", query="select u from StepsToHelpPrepare u order by u.position ASC")
 public class StepsToHelpPrepare {
 
     @Id
@@ -16,5 +17,6 @@ public class StepsToHelpPrepare {
     public String name;
     public String subTitle;
     public String description;
+    public Integer position;
 
 }
