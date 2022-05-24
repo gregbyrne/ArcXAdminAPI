b82244a012c60746a9b83f241338a57465c8033c
gregbyrne <byrne.gregory@epa.gov>
2022-05-24 16:11:18 -0400

    Create create database.sql

---
+++ sql/create database.sql
@@ -0,0 +1,12 @@
+-- Database: arcx
+
+-- DROP DATABASE arcx;
+
+CREATE DATABASE arcx
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
